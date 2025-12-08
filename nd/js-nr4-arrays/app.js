affe1a44b8f8d4148c99b7e8aa2662d70d3f2ca0
Arnas-Projects <coding.myprojects@gmail.com>
2025-12-08 04:37:38 +0200

    JavaScript. Arrays Tasks. 4 taks completed.

--- nd/js-nr4-arrays/app.js
+++ nd/js-nr4-arrays/app.js
@@ -19,22 +19,119 @@ const A = [
     'Naminis katinas',
 ];
 
-const UlTask1 = document.querySelector('ul');
+const UlTask1 = document.querySelector('ul.task1-ul');
 
-for (let i = 0; i < 9; i++) {
+// 1-as variantas ---> naudojant for ciklą
 
+// for (let i = 0; i < 9; i++) {
+
+//     const list = document.createElement('li');
+//     list.innerText = A[i];
+//     UlTask1.appendChild(list);
+// }
+
+
+// 2-as variantas ---> naudojant forEach metodą
+
+A.forEach(gyvuliukas => {
     const list = document.createElement('li');
-    list.innerText = A[i];
+    list.innerText = gyvuliukas;
     UlTask1.appendChild(list);
-}
+});
 
 
 // Task 2 -----------------------------------------------------------------------------------------------------
+
 // Html faile sukurkite ul tagą (tiesiogiai). 
 // Į sukurtą tagą, su JS, sudėkite li tagus su iš masyvo A nuskaitytais 
 // gyvūnais išdėliotais abėcėline tvarka.
 
 console.log('\nTask 2');
+
+// 1 etapas -----------------
+const UlTask2 = document.querySelector('ul.task2-ul');
+
+const SortedArrayAByABC = A.toSorted((a, b) => {
+    return a.localeCompare(b);
+});
+
+
+// 2 etapas -----------------
+SortedArrayAByABC.forEach(gyvunas => {
+
+    const listukas = document.createElement('li');
+    listukas.innerText = gyvunas;
+    UlTask2.appendChild(listukas);
+});
+
+
+
+// Task 3 -----------------------------------------------------------------------------------------------------
+
+// Html faile sukurkite ul tagą (tiesiogiai). 
+// Į sukurtą tagą, su JS, sudėkite li tagus su iš masyvų A ir B 
+// nuskaitytais gyvūnais išdėliotais abėcėline tvarka 
+// (Rūšiavimas turi būti bendras tarp abiejų masyvų, 
+// NE kiekvieno atskirai).#pamastykKaip
+
+console.log('\nTask 3');
+
+const B = [
+    'Kalakutas',
+    'Višta',
+    'Antis',
+    'Žąsis',
+    'Ančiasnapis'
+];
+
+
+// 1 etapas -----------------
+const CombinedAB = [...A, ...B];
+
+console.log(CombinedAB);
+
+
+// 2 etapas -----------------
+const UlTask3 = document.querySelector('ul.task3-ul');
+
+const CombinedABsorted = CombinedAB.toSorted((a, b) => {
+    return a.localeCompare(b);
+});
+
+console.log(CombinedABsorted);
+
+
+// 3 etapas -----------------
+CombinedABsorted.forEach(zveriukas => {
+
+    const listukas = document.createElement('li');
+    listukas.innerText = zveriukas;
+    UlTask3.appendChild(listukas);
+});
+
+
+// Task 4 -----------------------------------------------------------------------------------------------------
+
+// Html faile sukurkite section tagą (tiesiogiai). 
+// Į sukurtą tagą, su JS, sudėkite div tagus, 
+// kurie yra mėlyni apskritimai su centre užrašytais 
+// iš masyvo A nuskaitytais gyvūnais.
+
+console.log('\nTask 4');
+
+const section4 = document.querySelector('section.task4-sect');
+
+
+A.forEach(zveriukas => {
+
+    const divas = document.createElement('div');
+    divas.innerText = zveriukas;
+    divas.classList.add('task4-div')
+    section4.appendChild(divas);
+});
+
+
+
 
 
 
---
+++ programming-practice/js-practice/js-024-02-arrays/app.js
@@ -0,0 +1,281 @@
+
+
+
+// fetch('https://in3.dev/inv/') // siunčia užklausa
+//     .then(res => res.json()) // laukiam tada JSON gautą rezultatą
+//     .then(inv => console.log(inv)); // kai yra JSON tada loginam
+
+
+
+const invoiceJson = `{
+    "number": "AB-53736408",
+    "date": "2025-11-27",
+    "due_date": "2025-12-08",
+    "company": {
+        "buyer": {
+            "name": "UAB Kazlauskas ir Urbonas",
+            "address": "Laisvės gatvė 79-73, Telšiai",
+            "code": "19271836",
+            "vat": "LT601046378",
+            "phone": "863848197",
+            "email": "nina68@gedminas.lt"
+        },
+        "seller": {
+            "name": "UAB Jankauskas ir Kavaliauskas",
+            "address": "Žalgirio g. 44, Jonava LT-43157",
+            "code": "96103682",
+            "vat": "LT711569442",
+            "phone": "+37066376540",
+            "email": "vasiliauskiene.sigita@osmundas.lt"
+        }
+    },
+    "items": [
+        {
+            "description": "Komoda su papildoma lentyna",
+            "quantity": 1,
+            "price": 330,
+            "discount": []
+        },
+        {
+            "description": "Kampinė odinė sofa Ganykla su patalynės dėže",
+            "quantity": 2,
+            "price": 660,
+            "discount": []
+        },
+        {
+            "description": "Sulakstoma ąžuolinė kėdė Barbaras su uždengimu",
+            "quantity": 1,
+            "price": 90.5,
+            "discount": []
+        },
+        {
+            "description": "Komoda žalia su 4 stalčiais",
+            "quantity": 6,
+            "price": 240,
+            "discount": []
+        },
+        {
+            "description": "Kėdė Violeta su uždengimu",
+            "quantity": 2,
+            "price": 60.97,
+            "discount": []
+        }
+    ],
+    "shippingPrice": 48.5
+}`;
+
+
+const invoice = JSON.parse(invoiceJson);
+
+console.log(invoice);
+
+console.log('\n');
+
+// Arrays Part 5 ----------------------------------------------------------------------------------------------------
+
+
+const animals = [
+    {
+        name: 'Briedis',
+        age: 54,
+        fur: true
+    },
+    {
+        name: 'Lapė',
+        age: 12,
+        fur: true
+    },
+    {
+        name: 'Vilkas',
+        age: 8,
+        fur: true
+    },
+    {
+        name: 'Kiškis',
+        age: 4,
+        fur: true
+    },
+    {
+        name: 'Erelis',
+        age: 16,
+        fur: false
+    },
+    {
+        name: 'Voverė',
+        age: 3,
+        fur: true
+    }
+];
+
+
+// 1. forEach --- Pačiupinėjam visus elementus iš eilės ===> forEach
+
+
+animals.forEach(zveris => {
+    console.log(zveris.name);
+});
+
+
+
+// 2. MAP --- Sukuriam patobulintą masyvą iš seno masyvo ===> map
+
+
+
+const PaseneZverys = animals.map(zveris => {
+    return { ...zveris, age: zveris.age + 1 };  // ...zveris - yra kopija seno objekto, age : užrašome ant viršaus naują age
+});
+
+console.log(PaseneZverys);
+
+
+// 3. FIND --- Surandame masyve tai, kas mus domina ===> find
+
+const wolf = animals.find(zveriukas => {
+    if (zveriukas.name == 'Vilkas') {
+        return true;  // paieška nutraukta, objektas su vilku rastas
+    }
+    return false;  // paieška eina prie sekančio objekto
+});
+
+console.log(wolf);
+
+// let suma = 0;
+
+
+// 4. REDUCE --- Suskaičiuojame kažką masyve ===> reduce
+
+// 1-as variantas ===> forEach
+
+// animals.forEach(zveriukas => {
+//     suma = suma + zveriukas.age;
+//     //arba: suma += zveriukas.age;
+// });
+
+// console.log(suma);
+
+// 2-as variantas ===> reduce
+const allAges = animals.reduce((suma, zveriuks) => {
+    return suma + zveriuks.age; // įrašo naują 'suma' reikšmę
+}, 0); // 0 - pradinė 'suma' reikšmė nuo kurios pradedame skaičiuot. Panašiai, kaip let suma = 0;
+
+console.log(allAges);
+
+
+// 5. FILTER --- Naujo išfiltruoto masyvo sudarymas ===> filter
+
+const YoungAnimals = animals.filter(zveris => {
+    if (zveris.age <= 10) {
+        return true;  // jei tinka, pridedam prie išfiltruoto masyvo
+    } else {
+        return false;  // jei netinka, nededam
+    }
+});
+
+console.log(YoungAnimals);
+
+
+// 6. toSorted --- Naujo, surūšiuoto masyvo sudarymas ===> toSorted
+
+// 1-as variantas ---> paprastesnis, lengvesnis
+const SortedByAge = animals.toSorted((pirmas, antras) => {
+    return pirmas.age - antras.name; // jeigu 'pirmas' > 'antras' - grąžins teigiamą skaičių, jeigu 'pirmas' < 'antras' - grąžins neigiamą
+});
+
+// console.log(SortedByAge);
+
+
+
+//2-as variantas ---> truputį sudėtingesnis
+const SortedByAge2 = animals.toSorted((a, b) => {
+    if (a.age > b.age) {
+        return -1;   // sukeičiam vietomis skaičius ir turime rūšiavimą iš priešingos pusės
+    }
+    if (a.age < b.age) {
+        return 1;  // sukeičiam vietomis skaičius ir turime rūšiavimą iš priešingos pusės
+    }
+    return 0;
+});
+
+console.log(SortedByAge, SortedByAge2);
+
+
+
+// 3-as variantas ---> Rūšiavimas pagal abėcėlę
+const SortedByABC = animals.toSorted((pirmas, antras) => {
+    return pirmas.name.localeCompare(antras.name); // jeigu 'pirmas' > 'antras' - grąžins teigiamą skaičių, jeigu 'pirmas' < 'antras' - grąžins neigiamą
+});
+
+console.log(SortedByABC);
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
