39bc222001e58e1b2434de80e7ee3a8576990e98
Arnas-Projects <coding.myprojects@gmail.com>
2025-12-15 17:10:39 +0200

    030 paskaita PART 2 - CRUD - [U]pdate (edit)

--- 030/app.js
+++ 030/app.js
@@ -56,12 +56,51 @@ const render = _ => {
         const rowHtml = listRowTemplate.content.cloneNode(true);
         const colorSq = rowHtml.querySelector('[data-color-sq]');
 
+        //****DELETE */
+
+        const deleteButton = rowHtml.querySelector('[data-delete-sq]');
+        
+        // colorItem.id - sugeneruotas Store metode id
+        // dataset.id // į elementą prideda atributą "data-id"
+        // dataset.id = colorItem.id - atributui priskiria reikšmę data-id="213246848"
+
+        deleteButton.dataset.id = colorItem.id;  
+
+        deleteButton.addEventListener('click', e => {
+
+            // e eventas
+            // e.target iš evento gautas paspaustas mygtuko elementas
+            // e.target.dataset kreipimasis į elemento "data-" atributus
+            // e.target.dataset.id kreipimasis į atributą "data-id"
+
+            const id = parseInt(e.target.dataset.id);
+            Destroy(id);
+        });
+
+        //***EDIT */
+
+        const editInput = rowHtml.querySelector('[data-edit-color-input]');
+        const editButton = rowHtml.querySelector('[data-edit-color-button]');
+
+        editInput.value = colorItem.color;  // senų duomenų perrašymas į edit formą
+        editButton.dataset.id = colorItem.id;
+
+        editButton.addEventListener('click', e => {
+
+            const id = parseInt(e.target.dataset.id);
+            const color = editInput.value;
+
+            Update(id, color);
+        });
+
+
+
         colorSq.style.backgroundColor = colorItem.color + '70'; // + permatomumo skačius
         colorSq.style.borderColor = colorItem.color; 
 
         listBin.appendChild(rowHtml);
     });
-}
+};
 
 /*
     Store funkcija vyko naujo "daikto" įrašymą į saugyklą
@@ -71,12 +110,12 @@ const render = _ => {
 
 
 const Store = data => {
-    const id = rand(10000000, 9999999);  // netikras unikalus id
+    const id = rand(10000000, 99999999);  // netikras unikalus id
     dataToStore = {
         id, // ===> supaprastintas id: id - kadangi vardas ir reikšmė toks pats
         color: data,
     }
-    LIST.push(dataToStore);
+    LIST.unshift(dataToStore);
     writeLocalStorage();
     render();
 };
@@ -84,28 +123,30 @@ const Store = data => {
 init();
 
 
+/*
+    Destroy() įvykdo "daikto" pašalinimą iš saugyklos
+    Šis metodas turi gauti "daikto" indentifikatorių (id)
+    Turi pašalinti daiktą su nurodytu identifikatorium
+*/
+
+const Destroy = id => {
+    LIST = LIST.filter(color => color.id != id);  // išmetam iš listo kvadratuką su nurodytu id
+    writeLocalStorage();
+    render();
+};
 
 
+/*
+    Update() vykdo redaguoto "daikto" saugojimą saugykloje
+    Šis metodas turi gauti "daikto" indentifikatorių (id) ir "daikto" naujas savybes
+    Turi persaugoti "daiktą" su nurodytu (id) ir naujomis savybėmis
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const Update = (id, data) => {
+    LIST = LIST.map(item => item.id == id ? {...item, color: data} : item);
+    writeLocalStorage();
+    render();
+};
 
 
 
