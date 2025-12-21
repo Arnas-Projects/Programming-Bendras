6ea927099a217d6db87b3648f90d1c60e627cc4f
Arnas-Projects <coding.myprojects@gmail.com>
2025-12-21 23:56:14 +0200

    JS namų darbai NR 6. OOP - object oriented programming. Atliktos 2 užduotys.

---
+++ nd/js-nr6-OOP/app.js
@@ -0,0 +1,140 @@
+
+
+// ---------------------------------------------------------------------------------------------------------
+// Sukurti klasę Kibiras1. Konstruktoriuje sukurti savybę 
+// akmenuKiekis  kuri lygi 0. Parašyti šiai klasei metodus, 
+// pridedančius akmenis: prideti1Akmeni() pridetiDaugAkmenu(kiekis) 
+// ir metodą išvedantį akmenų kiekį į konsolę- kiekPririnktaAkmenu(). 
+// Sukurti vieną kibiro objektą ir pademonstruoti akmenų rinkimą į kibirą 
+// ir rezultatų išvedimą.
+console.log('************ Task 1 ************');
+
+
+
+class Kibiras1 {
+
+    constructor() {
+        this.akmenuKiekis = 0;
+    }
+
+
+    prideti1Akmeni() {
+    this.akmenuKiekis++;
+    }
+
+
+    pridetiDaugAkmenu(kiekis) {
+        this.akmenuKiekis += kiekis
+    }
+
+
+    kiekPririnktaAkmenu() {
+        console.log(`Akmenų kiekis: ${this.akmenuKiekis}`)
+    }
+}
+
+const kibirelis = new Kibiras1();
+
+console.log(kibirelis);
+
+kibirelis.prideti1Akmeni();
+kibirelis.prideti1Akmeni();
+console.log(kibirelis);
+
+kibirelis.pridetiDaugAkmenu(5);
+console.log(kibirelis);
+
+kibirelis.kiekPririnktaAkmenu();
+
+kibirelis.prideti1Akmeni();
+kibirelis.prideti1Akmeni();
+
+kibirelis.kiekPririnktaAkmenu();
+
+
+// ---------------------------------------------------------------------------------------------------------
+// Sukurti klasę Pinigine. Konstruktoriuje sukurti dvi savybes 
+// popieriniaiPinigai ir metaliniaiPinigai. Parašyti metodą ideti(kiekis), 
+// kuris prideda pinigus į piniginę. Jeigu kiekis nedidesnis už 2, tai prideda 
+// prie metaliniaiPinigai, jeigu kitaip- prie popieriniaiPinigai. 
+// Parašykite metodą skaiciuoti(), kuris suskaičiuotų ir išvestų į 
+// konsolę popieriniaiPinigai ir metaliniaiPinigai sumą. Sukurti klasės 
+// objektą ir pademonstruoti veikimą. Nesvarbu kokios popierinės kupiūros 
+// ir metalinės monetos egzistuoja realiame pasaulyje.
+
+console.log('\n************ Task 2 ************');
+
+
+class Pinigine {
+    constructor() {
+        this.popieriniaiPinigai = 0;
+        this.metaliniaiPinigai = 0;
+    }
+
+
+    ideti(kiekis) {
+        
+        // let piniguKiekis = PinigaiPinigineje
+
+        if (kiekis <= 2) {
+            this.metaliniaiPinigai = this.metaliniaiPinigai + kiekis;
+        } else {
+            this.popieriniaiPinigai = this.popieriniaiPinigai + kiekis;
+        }
+    }
+
+
+    skaiciuoti() {
+
+        const suma = this.popieriniaiPinigai + this.metaliniaiPinigai;
+        console.log(`Dabar piniginėje yra: ${suma} pinigų`);
+    }
+}
+
+const PinigaiPinigineje = new Pinigine();
+
+console.log(PinigaiPinigineje);
+
+PinigaiPinigineje.ideti(3);
+PinigaiPinigineje.ideti(1);
+PinigaiPinigineje.ideti(2);
+
+console.log(PinigaiPinigineje);
+
+PinigaiPinigineje.skaiciuoti();
+
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+// Sukurti klasę Troleibusas. Konstruktoriuje sukurti savybę 
+// keleiviuSkaicius kuri yra lygi 0. Parašyti du metodus: 
+// ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius). 
+// O taip pat parašyti metoda vaziuoja(), kuris į konsolę 
+// išvestų troleibusu važiuojančių keleivių skaičių. 
+// Atkreipkite dėmesį, kad troleibusu važiuoti neigiamas 
+// keleivių skaičius negali.
+
+
+console.log('\n************ Task 3 ************');
+
+
+// class Troleibusas {
+
+//     constructor() {
+//         this.keleiviuSkaicius = 0;
+//     }
+
+//     ilipa(keleiviuSkaicius)
+// }
+
+
+
+
+
+
+
+
+
+
+
