a70c973bef55882161e7c0b45f0475c109188333
Arnas-Projects <coding.myprojects@gmail.com>
2025-12-30 13:37:19 +0200

    038 paskaita. OOP, paveldimumas, map & set

---
+++ obj/mapSet/app-set.js
@@ -0,0 +1,24 @@
+
+
+
+console.log('Set');
+console.log('\n');
+
+
+const set = new Set();
+
+set.add('red');
+set.add('blue');
+set.add('raccoon');
+set.add('red'); // setas yra unikalių reikšmių rinkinys
+
+console.log(set, set.size, set.has('red'), set.delete('red'), set.has('red'));
+
+// for in
+// for of
+
+for (let i = 0; i < arr.length; i++) {
+    
+}
+
+
