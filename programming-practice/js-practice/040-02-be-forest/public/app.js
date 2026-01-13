35b7616b194746d057bd6d8562b7154dee0ddd48
Arnas-Projects <coding.myprojects@gmail.com>
2026-01-13 19:16:02 +0200

    040 pask kartojimas. Back-end. HTML klijavimas iš atskirų dalių.

---
+++ programming-practice/js-practice/040-02-be-forest/public/app.js
@@ -0,0 +1,17 @@
+
+
+
+const button = document.querySelector('.btn');
+
+if (button) {
+    button.addEventListener('click', _ => {
+        console.log('Button clicked!');
+    });
+};
+
+
+
+
+
+
+
