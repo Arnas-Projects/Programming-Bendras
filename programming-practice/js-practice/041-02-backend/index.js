aa4646242a4f38b6670c3e09b8b683734c820e66
Arnas-Projects <coding.myprojects@gmail.com>
2026-01-13 23:21:41 +0200

    041 pask kartojimas. Back-end. URL parametrai.

--- programming-practice/js-practice/041-02-backend/index.js
+++ programming-practice/js-practice/041-02-backend/index.js
@@ -30,9 +30,18 @@ app.get('/about/raccoon/green', (req, res) => {
 });
 
 
+
+
+
 app.get('/url-symbols', (req, res) => {
 
-    res.send('Green raccoon page');
+    const symbols = ['!-----@-----#-----$-----%-----^-----&-----?-----=----- '];
+
+    const encodedSymbols = encodeURIComponent(symbols);
+
+    res.send('Decoded symbols: ' + decodeURIComponent(encodedSymbols) +
+        '<br>' + 'Encoded symbols: ' + encodedSymbols
+    );
 
 });
 
@@ -42,14 +51,79 @@ app.get('/url-symbols', (req, res) => {
 // Duomenų siuntimas su parametrais
 // parametrai yra URL dalis
 // :color yra kintamasis įdėtas į URL vietą
-// app.get('/moose/:color', (req, res) => {
-app.get('/:color/moose', (req, res) => {
+app.get('/moose/:color', (req, res) => {
+    // app.get('/:color/moose', (req, res) => {
     // iš URL paimame spalvos parametrą
-    const mooseColor = req.params.color;
-    
+    const mooseColor = req.params.color; // spalva yra automatiškai URL dekoduojamas
+
     res.send(`<h1 style="color:${mooseColor}"> Briedžio spalva yra: ${mooseColor}</h1>`);
 
 });
+
+
+
+app.get('/kas/:vardas/amzius/:kiek', (req, res) => {
+
+    const name = req.params.vardas;
+    const age = req.params.kiek;
+    res.send(`Mano vardas ${name}. Man yra: ${age} metų`);
+
+});
+
+
+// -------------------------------------------------------------------
+// Padaryti kalkuliatorių skaičiuotuvą sumai per URL skaičiuoti
+// URL pvz: /suma/5/10 => turi parašyti "5 + 10 = 15"
+
+app.get('/suma/:num1/:num2', (req, res) => {
+
+    // const pirmas = +req.params.num1; // alternatyva kaip stringą galima paversti skaičiumi
+    // const antras = +req.params.num2; // alternatyva kaip stringą galima paversti skaičiumi
+    const pirmas = Number(req.params.num1);
+    const antras = Number(req.params.num2);
+    const suma = pirmas + antras;
+    res.send(`Suma: ${pirmas} + ${antras} = ${suma}`);
+
+});
+
+
+// -------------------------------------------------------------------
+// Padaryti didesnio skaičiaus radimą per URL
+// pvz: /didesnis/5/10  => turi parašyti "Didesnis skaičius yra 10"
+
+app.get('/didesnis/:num1/:num2', (req, res) => {
+
+    const pirmas = Number(req.params.num1);
+    const antras = Number(req.params.num2);
+
+    let didesnis;
+
+    
+    // Vartotojo įvestos informacijos validacija (tikrinimas)
+
+    if (isNaN(pirmas) || isNaN(antras)) {
+        res.send(`Tai nėra skaičiai`);
+        return;
+    }
+
+    // Alternatyva su TERNARY
+    // pirmas > antras ? didesnis = pirmas : didesnis = antras;
+
+
+    if (pirmas > antras) {
+        didesnis = pirmas;
+    } else if (pirmas < antras) {
+        didesnis = antras;
+    } else if (pirmas == antras) {
+        res.send(`${pirmas} | ${antras} | Skaičiai yra lygūs`);
+        return;
+    }
+
+    res.send(`${pirmas} | ${antras} | Didesnis yra: ${didesnis}`);
+
+});
+
+
 
 
 
