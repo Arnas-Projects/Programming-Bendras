8d21e83d4542b0ea4aa34abc5c772fc852c36b4f
Arnas-Projects <coding.myprojects@gmail.com>
2026-01-15 09:19:56 +0200

    042 pask kartojimas. Back-end forest continue

---
+++ programming-practice/js-practice/042-02-backend2/index.js
@@ -0,0 +1,115 @@
+
+
+
+const express = require('express');
+const fs = require('fs'); // failų sistemos modulis - biblioteka
+const app = express();
+const port = 80;
+// importuojam duomenis iš 'services.js' failo
+const { services } = require('./services');
+
+// Failai folderyje 'public' bus pasiekiami per naršyklę
+app.use(express.static('public')); // Nurodome, kad statiniai failai bus iš 'public' folderio
+
+
+
+// Dalis Router
+
+// Home Page
+// ------------------------------------------------------------------------------------------------------------------
+app.get('/', (req, res) => {
+
+    // skaitome tris atskirus failus. Toks skaitymas gali būti tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const homepage = fs.readFileSync('./html/home.html', 'utf8');
+
+    const pageTitle = 'Home Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + homepage + bottom); // naršyklei atiduodam vieną "suklijuotą iš kelių" failą
+});
+
+
+
+// Service
+// ------------------------------------------------------------------------------------------------------------------
+app.get('/service/:slug', (req, res) => {
+    const slugas = req.params.slug;
+    const service = services.find(item => item.slug === slugas);
+
+    const serviceHTML = fs.readFileSync('./html/service.html', 'utf8');
+
+    // Pakeičiame vietas HTML faile su faktiniais duomenimis
+    let servicePage = serviceHTML.replace('{{icon}}', service.icon)
+        .replace('{{title}}', service.title)
+        .replace('{{description}}', service.description);
+
+    res.send(servicePage);
+});
+
+
+
+
+// Services
+// ------------------------------------------------------------------------------------------------------------------
+app.get('/services', (req, res) => {
+
+    // skaitome tris atskirus failus. Toks skaitymas gali būti tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const services = fs.readFileSync('./html/services.html', 'utf8');
+
+    const pageTitle = 'Services';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + services + bottom); // naršyklei atiduodam vieną "suklijuotą iš kelių" failą
+});
+
+
+
+// About Us
+// ------------------------------------------------------------------------------------------------------------------
+app.get('/about', (req, res) => {
+
+    // skaitome tris atskirus failus. Toks skaitymas gali būti tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const about = fs.readFileSync('./html/about.html', 'utf8');
+
+    const pageTitle = 'About Us';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + about + bottom); // naršyklei atiduodam vieną "suklijuotą iš kelių" failą
+});
+
+
+
+// Contact Us
+// ------------------------------------------------------------------------------------------------------------------
+app.get('/contact', (req, res) => {
+
+    // skaitome tris atskirus failus. Toks skaitymas gali būti tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const contact = fs.readFileSync('./html/contact.html', 'utf8');
+
+    const pageTitle = 'Contact Us';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + contact + bottom); // naršyklei atiduodam vieną "suklijuotą iš kelių" failą
+});
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+// Paleidžia serverį ir parašo terminale, kad viskas gerai.
+app.listen(port, () => {
+    console.log(`Viskas OK. Barsukas dirba ant ${port} porto.`);
+});
+
+
